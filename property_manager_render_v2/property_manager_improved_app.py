8e80564c5cab1b0558f1e9d548d917cc35f63719
rashged <142747161+rashged@users.noreply.github.com>
2025-08-31 19:31:17 +0400

    Delete property_manager_render_v2 directory

--- property_manager_render_v2/property_manager_improved_app.py
+++
@@ -1,4 +0,0 @@
-"""
-Full code for property_manager_improved_app.py goes here.
-Use the complete version provided earlier.
-"""
--- property_manager_render_v2/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn -w 4 -b 0.0.0.0:10000 property_manager_improved_app:app
